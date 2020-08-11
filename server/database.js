7a95c0b107e2c247d06cd52df4b53dc47387b840
Ernesto.EspinosaSosa <ernesto.espinosa.sosa@gmail.com>
2020-08-11 15:02:54 -0500

    update

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose').MongoClient;
 
-mongoose.connect(process.env.MyConnectionString || process.env.MONGODB_URI, function (err, db) {
+mongoose.connect('mongodb://cosmosdbmeanstack:o1BTWdJyokdRb4EvVEkbpEnyWLVjXxQBo4SJBGKalV2grwgyHFOs449eZpUswTmzKecrzwJHKQQFfbBJHm8mIw==@cosmosdbmeanstack.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cosmosdbmeanstack@', function (err, db) {
     db.close();
 });
 
