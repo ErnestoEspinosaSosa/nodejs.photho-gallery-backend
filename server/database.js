4b7de7953749b0ad8c0f891833b5774da96fde12
Ernesto.EspinosaSosa <ernesto.espinosa.sosa@gmail.com>
2020-08-11 17:20:30 -0500

    tttttttttttttttttttt

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MyConnectionString, { useMongoClient: true })
+mongoose.connect('mongodb://cosmosdbmeanstack:o1BTWdJyokdRb4EvVEkbpEnyWLVjXxQBo4SJBGKalV2grwgyHFOs449eZpUswTmzKecrzwJHKQQFfbBJHm8mIw==@cosmosdbmeanstack.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cosmosdbmeanstack@', { useMongoClient: true })
 .then(db => console.log("DB is connected"));
 
 module.exports = mongoose;
