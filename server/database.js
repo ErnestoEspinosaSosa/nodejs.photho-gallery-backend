ed9f8c8ca2c498b08369324d5717649edb56b56c
Ernesto.EspinosaSosa <ernesto.espinosa.sosa@gmail.com>
2020-08-11 12:42:55 -0500

    update connection string

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CUSTOMCONNSTR_MyConnectionString || process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(db => console.log("DB is connected"))
 .catch(err => console.log(err));
 
