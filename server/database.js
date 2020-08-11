9aa119059b03eb93c0ba5879306c206e3d07daa9
Ernesto.EspinosaSosa <ernesto.espinosa.sosa@gmail.com>
2020-08-11 14:45:16 -0500

    update db

--- server/database.js
+++ server/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.CUSTOMCONNSTR_MyConnectionString || process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CUSTOMCONNSTR_MyConnectionString || process.env.MONGODB_URI, function (err, db) {
+    db.close();
+});
 .then(db => console.log("DB is connected"))
 .catch(err => console.log(err));
 
