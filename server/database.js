727f6f9fd0c4f45195be7b93aa27286dc43d9359
Ernesto.EspinosaSosa <ernesto.espinosa.sosa@gmail.com>
2020-08-11 20:40:36 -0500

    wwwwwwwwwwwww

--- server/database.js
+++ server/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MyConnectionString,{ useNewUrlParser: true })
+mongoose.connect(process.env.CUSTOMCONNSTR_MyConnectionString,{ useNewUrlParser: true })
 .then(db => console.log("DB is connected"));
 
 module.exports = mongoose;
