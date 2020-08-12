bc6342af964d65bb45268f3b87614b2c12d828d5
Ernesto.EspinosaSosa <ernesto.espinosa.sosa@gmail.com>
2020-08-11 21:10:24 -0500

    pppppppppppp

--- server/database.js
+++ server/database.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.CUSTOMCONNSTR_MyConnectionString,{ useNewUrlParser: true })
+mongoose.connect('mongodb://cosmosdbmeanstack:o1BTWdJyokdRb4EvVEkbpEnyWLVjXxQBo4SJBGKalV2grwgyHFOs449eZpUswTmzKecrzwJHKQQFfbBJHm8mIw==@cosmosdbmeanstack.mongo.cosmos.azure.com:10255/?ssl=true&appName=@cosmosdbmeanstack@',
+{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
 .then(db => console.log("DB is connected"));
 
 module.exports = mongoose;
